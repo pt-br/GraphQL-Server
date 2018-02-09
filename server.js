d6c2025e9c49749db2447eeadb80223b61de9d10
pt-br <ltavares@yieldstreet.com>
2018-02-09 01:18:08 -0200

    Add a /schema route to print the schema

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import graphqlHTTP from 'express-graphql';
+import { printSchema } from 'graphql';
 import { Schema } from './data/schema';
 
 const app = express();
@@ -10,6 +11,10 @@ app.use('/graphql', graphqlHTTP({
   schema: Schema,
   graphiql: true
 }));
+
+app.get('/schema', (req, res) => {
+  res.type('text/plain').send(printSchema(Schema));
+});
 
 app.listen(GRAPHQL_PORT, () => console.log(
   `GraphQL Server is now running on port ${GRAPHQL_PORT}`
