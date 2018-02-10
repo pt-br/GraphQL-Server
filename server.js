4680b9ed72b06bc004aa80d1099a978987dda391
pt-br <ltavares@yieldstreet.com>
2018-02-10 19:28:22 -0200

    Allow CORS

--- server.js
+++ server.js
@@ -1,20 +1,18 @@
 import express from 'express';
+import cors from 'cors';
 import graphqlHTTP from 'express-graphql';
 import { printSchema } from 'graphql';
 import { Schema } from './data/schema';
 
-const app = express();
-
 const GRAPHQL_PORT = 8080;
 
+const app = express();
+
+app.use(cors());
 app.use('/graphql', graphqlHTTP({
   schema: Schema,
   graphiql: true
 }));
-
-app.get('/schema', (req, res) => {
-  res.type('text/plain').send(printSchema(Schema));
-});
 
 app.listen(GRAPHQL_PORT, () => console.log(
   `GraphQL Server is now running on port ${GRAPHQL_PORT}`
