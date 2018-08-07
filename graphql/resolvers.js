61f3ecb290a5818490fd7cbd54d20581313765e4
pt-br <ltavares@yieldstreet.com>
2018-08-06 21:56:22 -0300

    V2 apollo container WIP

---
+++ graphql/resolvers.js
@@ -0,0 +1,45 @@
+// Mock Data
+const authors = [
+  { id: 1, firstName: 'Tom', lastName: 'Coleman' },
+  { id: 2, firstName: 'Sashko', lastName: 'Stubailo' },
+  { id: 3, firstName: 'Mikhail', lastName: 'Novikov' },
+];
+
+const posts = [
+  { id: 1, authorId: 1, title: 'Introduction to GraphQL', votes: 2 },
+  { id: 2, authorId: 2, title: 'Welcome to Apollo', votes: 3 },
+  { id: 3, authorId: 2, title: 'Advanced GraphQL', votes: 1 },
+  { id: 4, authorId: 3, title: 'Launchpad is Cool', votes: 7 },
+];
+
+// Resolvers define the technique for fetching the types in the
+// schema.  We'll retrieve books from the "books" array above.
+const resolvers = {
+  Query: {
+    posts() {
+      return posts;
+    }
+  },
+  Mutation: {
+    upvotePost(_, { postId }) {
+      const post = posts.find(post => post.id === parseInt(postId, 10));
+      if (!post) {
+        throw new Error(`Couldn't find post with id ${postId}`);
+      }
+      post.votes += 1;
+      return post;
+    }
+  },
+  Author: {
+    posts(author) {
+      return posts.filter(post => post.authorId === author.id);
+    }
+  },
+  Post: {
+    author(post) {
+      return authors.find(author => author.id === post.authorId);
+    }
+  }
+};
+
+module.exports = resolvers;
